c7c032ca80570717261647ffcdc59251b01ccb2f
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2023-09-06 14:33:03 +0800

    Initial Commit

---
+++ MetroDemoApp/MetroDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroDemoAppApp.swift
+//  MetroDemoApp
+//
+//  Created by d&a-m-pro  on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MetroDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
